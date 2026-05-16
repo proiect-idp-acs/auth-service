c131bca067e716d0a63f08fd6fbbe500fa94e5d2
ananas70 <ana_stoica70@yahoo.com>
2026-05-16 20:42:42 +0300

    added cors configuration

--- main.py
+++ main.py
@@ -3,37 +3,47 @@ from pydantic import BaseModel
 import jwt
 import datetime
 from prometheus_fastapi_instrumentator import Instrumentator
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="PlayerAuth Service", description="Gestionează autentificarea și JWT")
+app = FastAPI(title="PlayerAuth Service", description="Gestionează autentificarea și JWT (RBAC)")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # În producție aici s-ar pune domeniul de frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+Instrumentator().instrument(app).expose(app)
 
-# Secret key pentru a semna token-urile jwt
 SECRET_KEY = "super_secret_tennis_key"
 
-# Schema pentru datele primite de la utilizator
 class LoginRequest(BaseModel):
     username: str
     password: str
 
-# Activam instrumentarea pentru monitorizare cu Prometheus
-Instrumentator().instrument(app).expose(app)
+# Baza de date simulată pentru utilizatori și rolurile lor
+USERS_DB = {
+    "arbitru1": {"password": "parola123", "role": "arbitru"},
+    "spectator1": {"password": "parola123", "role": "spectator"}
+}
 
 @app.get("/")
 def read_root():
-    return {"message": "PlayerAuth Service este activ"}
+    return {"message": "PlayerAuth Service este activ!"}
 
 @app.post("/api/auth/login")
 def login(request: LoginRequest):
-    # Test user pentru arbitru
-    if request.username == "arbitru1" and request.password == "parola123":
-        # Generam token-ul JWT valabil 1h
-        payload = {
-            "sub": request.username,
-            "role": "arbitru",
-            "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
-        return {"access_token": token, "token_type": "bearer"}
+    user = USERS_DB.get(request.username)
     
-    raise HTTPException(status_code=401, detail="Credențiale incorecte")
-
-# TODO: Adauga ruta pentru validarea token-ului JWT și extragerea rolului utilizatorului
+    if not user or user["password"] != request.password:
+        raise HTTPException(status_code=401, detail="Credențiale incorecte")
+        
+    # Generăm token-ul introducând ROLUL utilizatorului în interiorul său (Payload)
+    payload = {
+        "sub": request.username,
+        "role": user["role"],
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    }
+    
+    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    return {"access_token": token, "token_type": "bearer", "role": user["role"]}
